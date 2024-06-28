b81318b3e2c9a9fdef0f829152a03416912078d6
Nanotixx <166376014+Nanotixx@users.noreply.github.com>
2024-06-28 18:31:06 +0900

    Add files via upload

---
+++ ASCIIDecConverter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Made By: Nanotixx
+//Simple Program I Made
+
+int main(){
+  char ASCII;
+
+  printf("Letters, Numbers and Symbols: ");
+  scanf("%c", &ASCII);
+  printf("ASCII Dec Value: %d", ASCII);
+
+};
